f8e24816609fdd9d9cf104f31e9ad92f2fcbe3a0
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2025-03-15 22:43:08 -0400

    chore: create HOME directory

--- launch.sh
+++ launch.sh
@@ -5,13 +5,13 @@ PAK_NAME="$(basename "$PAK_DIR")"
 PAK_NAME="${PAK_NAME%.*}"
 [ -f "$USERDATA_PATH/$PAK_NAME/debug" ] && set -x
 
-cd "$PAK_DIR" || exit 1
-
 rm -f "$LOGS_PATH/$PAK_NAME.txt"
 exec >>"$LOGS_PATH/$PAK_NAME.txt"
 exec 2>&1
 
 echo "$0" "$@"
+cd "$PAK_DIR" || exit 1
+mkdir -p "$USERDATA_PATH/$PAK_NAME"
 
 echo "1" >/tmp/stay_awake
 trap "rm -f /tmp/stay_awake" EXIT INT TERM HUP QUIT
